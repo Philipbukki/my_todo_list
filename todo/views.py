516fdeb4cbd353961a830159226a256895c8bbc8
Philip Bukki <philipbukkie@gmail.com>
2021-01-23 12:47:04 +0300

    My first commit

---
+++ todo/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
+
+
+def signupuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                (login, user)
+                return redirect('current_todos')
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html',
+                              {'form': UserCreationForm(),
+                               'error': 'That username has already been taken please choose another username'})
+        else:
+            return render(request, 'todo/signupuser.html',
+                          {'form': UserCreationForm(), 'error': 'Passwords did not match'})
+
+
+def current_todos(request):
+    return render(request, 'todo/current_todos.html')
